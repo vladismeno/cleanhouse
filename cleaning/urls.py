0c8b166a4256383ad0cde18cef3fe7d8831fc28f
vladismeno <vladismeno@gmail.com>
2024-06-01 13:13:39 -0700

    SEO optimize

--- cleaning/urls.py
+++ cleaning/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 
