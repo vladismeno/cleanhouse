dcea47d1973ac47553e07a1b6d50443a7be74904
owner <vladismeno@gmail.com>
2024-06-14 05:39:15 +0900

    test

--- cleanhouse/urls.py
+++ cleanhouse/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
