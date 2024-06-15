10b5c1ddf51980c8400b859586ce7c8751f5bfbd
owner <vladismeno@gmail.com>
2024-06-15 00:04:03 -0700

    описание проекта

--- cleanhouse/settings.py
+++ cleanhouse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DJANGO_DEBUG')
-DEBUG = False
+DEBUG = True
 
 print(DEBUG)
 
