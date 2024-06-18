8656383a0ef0f4b066c16742b5d50ec3d9b8326b
owner <vladismeno@gmail.com>
2024-06-18 01:52:42 -0700

    описание проекта

--- cleanhouse/settings.py
+++ cleanhouse/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8q^tpixw1go4@uk5a6q0s7+*b)(tltvi^b**%cffzhmm54lef#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-print(DEBUG)
+print(SECRET_KEY)
 
 ALLOWED_HOSTS = [
     'localhost',
