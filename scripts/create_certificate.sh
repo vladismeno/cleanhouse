765587ccba2350af08c60d68a0d3857dfc577fc2
owner <vladismeno@gmail.com>
2024-06-19 12:49:28 -0700

    описание проекта

--- scripts/create_certificate.sh
+++ scripts/create_certificate.sh
@@ -7,4 +7,4 @@ docker run -it --rm --name certbot \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "/root/cleanhouse:/var/www/html" \
     certbot/certbot certonly --webroot -w /var/www/html \
-    -d cleanhouse4you.com -d www.cleanhouse4you.com --force-renewal -v
+    -d www.cleanhouse4you.com --force-renewal -v
