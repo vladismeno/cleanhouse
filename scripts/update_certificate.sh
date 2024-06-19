40714e1a6f90c27fa706aea40d49f8a784e86227
owner <vladismeno@gmail.com>
2024-06-19 14:47:39 -0700

    описание проекта

--- scripts/update_certificate.sh
+++ scripts/update_certificate.sh
@@ -5,8 +5,7 @@ docker run --rm --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
     -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
     -v "/root/cleanhouse:/var/www/html" \
-    certbot/certbot renew --quiet --config /etc/letsencrypt/renewal/www.cleanhouse4you.com.conf --force-renewal
-
+    certbot/certbot renew --quiet --config /etc/letsencrypt/renewal/www.cleanhouse4you.com.conf
 
 # Перезапуск Nginx через docker-compose
 docker-compose -f /root/cleanhouse/docker-compose.yml restart nginx
