ffa2883871cc50b2ceb293e8a6ec5a79ab8ef953
vladismeno <vladismeno@gmail.com>
2024-11-20 11:59:48 -0800

    change link instagram

--- scripts/create_certificate.sh
+++ scripts/create_certificate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # нужно запускать когда сайт поднят www.cleanhouse4you.com cleanhouse4you.com и работать хотя бы по http протоколу
+# так что сначала нужно скопировать папку /etc/letsencrypt/ (она есть в проекте) на сервер digital ocean
+# в файле nginx.conf указаны пути к сертификатам
 
 docker run -it --rm --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
